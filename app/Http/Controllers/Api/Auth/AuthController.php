26342f10c66b35d5f537eb692b50188dc17f6b04
José Alarcón <joseq.ja@gmail.com>
2023-09-19 13:18:14 -0500

    Modified auth controller for work using JWT

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Api\Auth;
 
+use App\DTOs\TokenDTO;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\Exceptions\JWTException;
 
 class AuthController extends Controller
 {
@@ -16,20 +18,31 @@ class AuthController extends Controller
 
         $credentials = $request->only('username', 'password');
 
-        if (auth()->attempt($credentials)) {
-            $user = auth()->user();
-            $token = $user->createToken('access_token');
-            $minutesToExpire = $token->token->expires_at->diffInMinutes(now());
-
-            return response()->json([
-                'token' => $token->accessToken,
-                'minutes_to_expire' => $minutesToExpire + 1,
-            ], 200);
-
-        } else {
-            return response()->json([
-                'errors' => 'Password incorrect for: '.$request->username,
-            ], 401);
+        try {
+            if (! $token = auth()->attempt($credentials)) {
+                return response()->json(['errors' => 'Password incorrect for: '.$request->username], 401);
+            }
+        } catch (JWTException $e) {
+            return response()->json(['errors' => $e], 500);
         }
+
+        $reponse = ([
+            'token' => $token,
+            'minutes_to_expire' => auth()->factory()->getTTL(),
+        ]);
+
+        $response = TokenDTO::from($reponse);
+
+        return response()->json($response);
+    }
+
+    /**
+     * Get the authenticated User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function profile()
+    {
+        return response()->json(auth()->user());
     }
 }
