18c7caba8c2d1535626f8b114db3b92c5de5f85c
José Alarcón <joseq.ja@gmail.com>
2023-09-18 22:45:46 -0500

    Login controller

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Api\Auth;
 
 use App\Http\Controllers\Controller;
-use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
@@ -11,40 +10,26 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         $request->validate([
-            'username' => ['required', 'string', 'max:50'],
-            'password' => ['required', 'string', 'max:50'],
+            'username' => ['required', 'string'],
+            'password' => ['required', 'string'],
         ]);
 
-        $credentials = [
-            'username' => $request->username,
-            'password' => $request->password,
-        ];
+        $credentials = $request->only('username', 'password');
 
         if (auth()->attempt($credentials)) {
             $user = auth()->user();
             $token = $user->createToken('access_token');
-            $expiresAt = Carbon::parse($token->token->expires_at);
-            $now = Carbon::now();
-            $minutesToExpire = $now->diffInMinutes($expiresAt);
+            $minutesToExpire = $token->token->expires_at->diffInMinutes(now());
 
             return response()->json([
-                'meta' => [
-                    'success' => true,
-                    'errors' => [],
-                ],
-                'data' => [
-                    'token' => $token->accessToken,
-                    'minutes_to_expire' => $minutesToExpire + 1,
-                ],
+                'token' => $token->accessToken,
+                'minutes_to_expire' => $minutesToExpire + 1,
             ], 200);
+
         } else {
             return response()->json([
-                'meta' => [
-                    'success' => false,
-                    'errors' => ['Password incorrect for: '.$request->username],
-                ],
-                'data' => null,
-            ], 200);
+                'errors' => 'Password incorrect for: '.$request->username,
+            ], 401);
         }
     }
 }
