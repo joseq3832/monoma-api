f7c3a77c06e5e09fcb7f679a8076736617810541
José Alarcón <joseq.ja@gmail.com>
2023-09-20 21:04:05 -0500

    Add role middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,5 +65,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'auth.jwt' => \App\Http\Middleware\JwtMiddleWare::class,
+        'manager' => \App\Http\Middleware\RoleMiddleware::class,
     ];
 }
---
+++ app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class RoleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $role = auth()->payload()->get('role');
+
+        if ($role === 'manager') {
+            return $next($request);
+        }
+
+        return response()->json(['error' => 'Unauthorized'], 403);
+    }
+}
